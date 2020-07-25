2db0e1077303382aa165338c6dbdb6152dce0c2a
ErickMedinaLavielle <erickmedlav@gmail.com>
2020-07-24 19:30:08 -0500

    Updates

---
+++ precache-manifest.70f9afe11438d2237b0867f179680a55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e84998b5f5285ee55095620c766b45d6",
+    "url": "/github-jobs-react-project/index.html"
+  },
+  {
+    "revision": "b1d92012b8aa952b179d",
+    "url": "/github-jobs-react-project/static/css/main.00e3bc9d.chunk.css"
+  },
+  {
+    "revision": "79bb81f9a7c74f2b9a99",
+    "url": "/github-jobs-react-project/static/js/2.7aa43f74.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/github-jobs-react-project/static/js/2.7aa43f74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1d92012b8aa952b179d",
+    "url": "/github-jobs-react-project/static/js/main.7ec498d5.chunk.js"
+  },
+  {
+    "revision": "803306a4597dc116e67f",
+    "url": "/github-jobs-react-project/static/js/runtime-main.a0c094e4.js"
+  }
+]);
